e474a318b40753a88e45ab64b70b6465f0d6ab93
SylvesterTor <49233256+SylvesterTor@users.noreply.github.com>
2026-09-09 16:03:32 +0200

    code

--- src/conveyor-belt-capacity-check.c
+++ src/conveyor-belt-capacity-check.c
@@ -6,4 +6,12 @@ int main() {
     int motorCount, totalPackageWeight;
 
     /// YOUR CODE HERE
+    scanf("%d",&motorCount);
+    scanf("%d",&totalPackageWeight);
+    int totalMotorCapacity = motorCount*MOTOR_CAPACITY;
+    if(totalMotorCapacity>=totalPackageWeight){
+        printf("We have plenty of space");
+    }else{
+        printf("we do not have enough motors");
+    }
 }
